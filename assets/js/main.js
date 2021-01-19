ae657f0bce661dba5a76248dae50c41ace507694
Mirko <mirko.rinelli@gmail.com>
2021-01-19 12:42:43 +0100

    Html and js structures

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 new Vue ({
   el: '#root',
   data: {
-    
+    // data obj: Navbar
+    navBar: [],
+    // data obj: W section
+    wSection: [],
+    // data obj: Speakers section
+    speakers:[],
+    // data obj: Recent news
+    recentNews: [],
+    // data obj: Tickets
+    tickets: [],
+    // data obj: Sponsors
+    sponsors: [],
+    // data obj: footer
+    footer:[]
   }
 })

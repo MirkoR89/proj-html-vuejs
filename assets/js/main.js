a24c6c88e3761f622f394f51c914bd1cd6d4ddf1
Mirko <mirko.rinelli@gmail.com>
2021-01-19 15:53:17 +0100

    Working on Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,37 @@
 new Vue ({
   el: '#root',
   data: {
-    // data obj: Navbar
-    navBar: [],
+    // data obj: Navbar.
+    navBar: [
+      {
+        name: 'home',
+        link: null
+      },
+      {
+        name: 'program',
+        link: null
+      },
+      {
+        name: 'tickets',
+        link: null
+      },
+      {
+        name: 'speakers',
+        link: null
+      },
+      {
+        name: 'papers',
+        link: null
+      },
+      {
+        name: 'blog',
+        link: null
+      },
+      {
+        name: 'shortcodes',
+        link: null
+      }
+    ],
     // data obj: W section
     wSection: [],
     // data obj: Speakers section

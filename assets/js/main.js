181f1960d3664fdc035cd666969ba0b78ad1a382
Mirko <mirko.rinelli@gmail.com>
2021-01-20 14:34:53 +0100

    Working on slider conference and slider speakers

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,14 @@
 
 new Vue({
   el: '#root',
+
+  created: function () {
+    setInterval(this.sliderConferencer, 3000);
+  },
+
   data: {
     // data obj: Navbar.
-    navBar: [
-      {
+    navBar: [{
         name: 'home',
         link: null
       },
@@ -35,8 +39,7 @@ new Vue({
       }
     ],
     // data obj: Conferences
-    conferences: [
-      {
+    conferences: [{
         date: '23-26 may 2021',
         place: 'san francisco',
         speakers: '8 speakers'
@@ -52,9 +55,10 @@ new Vue({
         speakers: '9 speakers'
       }
     ],
+    //Counter to authoslide the conferences
+    counterConference: 0,
     // data obj: W section
-    wSection: [
-      {
+    wSection: [{
         icon: 'fas fa-trophy',
         title: 'who we are',
         paragraph: 'Donec id elit non mi porta gravida at eget me tus. Sed posuere consectetur estat lobo rtis. Cum sociis natoque.'
@@ -71,48 +75,49 @@ new Vue({
       }
     ],
     // data obj: Speakers section
-    speakers: [
-      {
-        profileImg:'speaker-1',
+    speakers: [{
+        profileImg: 'speaker-1',
         name: 'jonathan elves',
         role: "Twitter's Co-Founder"
       },
       {
-        profileImg:'speaker-2',
+        profileImg: 'speaker-2',
         name: 'matthew white',
         rolo: "Facebook's Co-Founder"
       },
       {
-        profileImg:'speaker-3',
+        profileImg: 'speaker-3',
         name: 'angelina holy',
         role: "Maxii's Manager"
       },
       {
-        profileImg:'speaker-4',
+        profileImg: 'speaker-4',
         name: 'michael dover',
         role: "Starbuck's CEO"
       },
       {
-        profileImg:'speaker-5',
+        profileImg: 'speaker-5',
         name: 'Janet Jones',
         role: "Newyork Post's GM"
       },
       {
-        profileImg:'speaker-6',
+        profileImg: 'speaker-6',
         name: 'patric spancer',
         role: "S&P Analyzer"
       },
       {
-        profileImg:'speaker-7',
+        profileImg: 'speaker-7',
         name: 'jhon super doe',
         role: "Yahoo's HR"
       },
       {
-        profileImg:'speaker-8',
+        profileImg: 'speaker-8',
         name: 'laurence francis',
         role: "Google's UI Designer"
       }
     ],
+    //Counter to move the slider-cards
+    counterSpeaker: 0,
     // data obj: Recent news
     recentNews: [],
     // data obj: Tickets
@@ -122,5 +127,28 @@ new Vue({
     // data obj: footer
     footer: []
   },
-  methods: {}
+
+  methods: {
+    //Function to slide the conferences
+    sliderConferencer() {
+      this.counterConference++;
+      if (this.counterConference > this.conferences.length -1) {
+        this.counterConference = 0 ;
+      }
+
+    },
+    //Function to Carosual Speaker
+    prevSpeaker() {
+      this.counterSpeaker--;
+      if (this.counterSpeaker < 0) {
+        this.counterSpeaker = this.speakers.length - 1;
+      }
+    },
+    nextSpeaker() {
+      this.counterSpeaker++;
+      if (this.counterSpeaker > this.speakers.length - 1) {
+        this.counterSpeaker = 0;
+      }
+    }
+  }
 })
